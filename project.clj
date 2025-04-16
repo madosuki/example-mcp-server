0fa1ab1ba6875a44d5ba4f69a2b9dcc20d497bca
madosuki <yurikasamaaikatsu@gmail.com>
2025-04-16 22:38:09 +0900

    done implement stdio server

--- project.clj
+++ project.clj
@@ -6,6 +6,10 @@
   :repositories [["sonatype" {:url "https://oss.sonatype.org/content/repositories/releases" :update :always}]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.modelcontextprotocol.sdk/mcp "0.9.0"]
-                 [jakarta.servlet/jakarta.servlet-api "6.1.0"]]
+                 [jakarta.servlet/jakarta.servlet-api "6.1.0"]
+                 [org.eclipse.jetty/jetty-server "12.0.19"]
+                 [org.eclipse.jetty.ee10/jetty-ee10-servlet "12.0.19"]]
   :repl-options {:init-ns my-mcp.core}
-  :main my-mcp.core/main)
+  :profiles {:uberjar {:aot :all}}
+  :aot [my-mcp.core]
+  :main my-mcp.core)
--- src/my_mcp/core.clj
+++ src/my_mcp/core.clj
@@ -1,27 +1,90 @@
 (ns my-mcp.core
-  (:import (io.modelcontextprotocol.server McpServer)
+  (:gen-class)
+  (:import (java.util.function BiFunction)
+           (io.modelcontextprotocol.server McpServer)
+           (io.modelcontextprotocol.server McpServerFeatures)
+           (io.modelcontextprotocol.server McpServerFeatures$SyncPromptSpecification)
            (io.modelcontextprotocol.spec McpSchema)
+           (io.modelcontextprotocol.spec McpSchema$Prompt)
+           (io.modelcontextprotocol.spec McpSchema$GetPromptResult)
+           (io.modelcontextprotocol.spec McpSchema$PromptArgument)
+           (io.modelcontextprotocol.spec McpSchema$PromptMessage)
+           (io.modelcontextprotocol.spec McpSchema$Role)
+           (io.modelcontextprotocol.spec McpSchema$TextContent)
            (io.modelcontextprotocol.spec McpSchema$ServerCapabilities)
            (io.modelcontextprotocol.server.transport StdioServerTransportProvider)
            (io.modelcontextprotocol.server.transport HttpServletSseServerTransportProvider)
-           (com.fasterxml.jackson.databind ObjectMapper)))
+           (com.fasterxml.jackson.databind ObjectMapper)
+           (org.eclipse.jetty.ee10.servlet ServletContextHandler)
+           (org.eclipse.jetty.ee10.servlet ServletHolder)
+           (org.eclipse.jetty.server Server)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-java-hash-map-with-initialize [l]
+  (let [r (new java.util.HashMap)]
+    (for [[k v] l]
+      (.put r k v))
+    r))
 
-(defn main []
-  (foo 'neko)
-  (let [server-capabilities (.build (.prompts (McpSchema$ServerCapabilities/builder) true))
+(defn generate-prompt-arguments []
+  (let [arg (McpSchema$PromptArgument. "arg1" "arg1 desc" false)
+        l (java.util.ArrayList.)]
+    (.add l arg)
+    l))
+
+(defn generate-prompt-result [desc msg]
+  (let [l (java.util.ArrayList.)]
+    (.add l msg)
+    (McpSchema$GetPromptResult. desc l)))
+
+(defn run-http-server [transport port]
+  (let [servlet-context-handler (new ServletContextHandler ServletContextHandler/SESSIONS)
+        servlet-holder (new ServletHolder transport)
+        http-server (new Server port)]
+    (.setContextPath servlet-context-handler "/")
+    (.addServlet servlet-context-handler servlet-holder "/*")
+    (.setHandler http-server servlet-context-handler)
+    (.start http-server)))
+
+(defn -main [& args]
+  (let [server-capabilities (.build (.prompts
+                                     (McpSchema$ServerCapabilities/builder)
+                                     true))
         json-mapper (new ObjectMapper)        
-        transport (new HttpServletSseServerTransportProvider json-mapper "/mcp/message" "/mcp/sse")
-        server (.build
-                (.capabilities
-                 (.serverInfo
-                  (McpServer/sync transport)
-                  "localhost"
-                  "0.0.1")
-                 server-capabilities))
+        transport-for-http (new HttpServletSseServerTransportProvider json-mapper "/mcp/message" "/mcp/sse")
+        transport (new StdioServerTransportProvider)
+        ;; prompts (McpSchema$Prompt. "name"
+        ;;                            "description"
+        ;;                            (generate-prompt-arguments))
+        prompts (McpSchema$Prompt. "name"
+                                   "description"
+                                   (java.util.ArrayList.))
+        prompts-specification (McpServerFeatures$SyncPromptSpecification.
+                               prompts
+                               (reify BiFunction (apply [this exchange request]
+                                                   (let [content (McpSchema$TextContent. "あなたはずんだもんです")
+                                                         msg (McpSchema$PromptMessage. McpSchema$Role/USER content)]
+                                                     (generate-prompt-result "desc of prompt" msg)))))
+        prompts-map (create-java-hash-map-with-initialize [["key" prompts-specification]])
         ]
-    ))
+    (let [server-name "clojure-mcp"
+          server-version "0.0.1"
+          mcp-server (.build
+                      (.capabilities
+                       (.serverInfo
+                        (McpServer/sync transport)
+                        server-name
+                        server-version)
+                       server-capabilities))
+          ;; mcp-server (.build
+          ;;             (.prompts
+          ;;              (.capabilities
+          ;;               (.serverInfo
+          ;;                (McpServer/sync transport)
+          ;;                server-name
+          ;;                server-version)
+          ;;               server-capabilities)
+          ;;              prompts-map))
+          ]
+      (.addPrompt mcp-server prompts-specification)
+      ;; (println "stdio mode")
+      )))
